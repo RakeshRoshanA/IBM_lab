45a455cb03e9affd3b1d7cff05a00b23ad776e4d
RakeshRoshanA <allamrakeshroshan@gmail.com>
2026-09-11 10:17:43 +0530

    Task 3: Houshold Registration

--- EcoPoints_Recycling_Tracker/src/RecyclingManager.java
+++ EcoPoints_Recycling_Tracker/src/RecyclingManager.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.LocalDate;
 
 public class RecyclingManager {
     // Task 2: Create HashMap collection to map unique ID -> Household object
@@ -7,6 +8,23 @@ public class RecyclingManager {
 
     public RecyclingManager() {
         this.householdMap = new HashMap<>();
+    }
+
+
+    //Registering a household with id, naem, address and joindate
+    public void registerHousehold(String id, String name, String address) {
+        if(householdMap.containsKey(id)) {{
+            System.out.println("Household with ID " + id + " already exists.");
+            return;
+        }
+        
+        LocalDate joinDate = LocalDate.now();
+
+        // Task 3: Create a new Household object and add it to the HashMap
+        Household household = new Household(id, name, address, joinDate);
+        householdMap.put(id, household);
+
+        System.out.println("Household registered successfully: " + household + " Name: " + name);
     }
 
     // Getter for testing/verification
